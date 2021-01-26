3cc7964a7a8899f56889500d90f8967c2614edb8
smallgram <j@jshz.io>
2021-01-25 20:40:54 -0500

    handle error cases

--- extract.nim
+++ extract.nim
@@ -8,15 +8,19 @@ if not fileExists(filename):
   quit("could not find file: " & filename, -1)
 
 var x:XmlNode = filename.loadXml()
+if x.isNil:
+  quit("could not find file: " & filename, -1)
+
 var i:XmlNode = x.child("ImageList").child("Image")
+if i.isNil:
+  quit("could not find image data", -1)
+
 var a:XmlAttributes = i.attrs
 var img_data = i.innerText
-var img_name = a["name"]
-
+var img_name = if a.hasKey("name"): a["name"].addFileExt("png") else: "stamp.png"
 if img_data.len == 0:
   quit("error extracting the image data.", -1)
 
 # write the image data to a file
-var img_filename = if img_name isnot "": img_name.addFileExt("png") else: "stamp.png"
-writeFile(img_filename, decode(img_data))
+writeFile(img_name, decode(img_data))
 echo "file written."
